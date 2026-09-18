8d575538340918ac4fb7485c4b83b7bbb5072601
Jesus Villarreal <jv5655217@gmail.com>
2026-09-18 09:35:21 -0500

    Corregir carga de imagen de campaña

--- app/campana/page.tsx
+++ app/campana/page.tsx
@@ -647,8 +647,7 @@ export default function CampañaPage() {
 
           </section>
         {/* CIERRE */}
-     <section className="text-center border border-slate-200 border-b-4 border-b-cyan-600 p-6 sm:p-8 md:p-10 rounded-2xl bg-slate-50 shadow-sm">
-
+<section className="text-center border border-slate-200 border-b-4 border-b-cyan-600 p-6 sm:p-8 md:p-10 rounded-2xl bg-slate-50 shadow-sm">
   <h2 className="text-xl sm:text-2xl font-bold text-cyan-700">
     La seguridad digital es una competencia esencial del siglo XXI
   </h2>
@@ -658,7 +657,6 @@ export default function CampañaPage() {
     críticos y preparados para desenvolverse de manera segura en los
     entornos digitales actuales.
   </p>
-
 </section>
 
       </div>
